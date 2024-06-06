8a5d879062d4c64154ef2ed774cbb915b964bffa
tlechevalier <thomas.lechevalier.ext@inetum.com>
2024-06-06 09:12:08 +0200

    Eslint settings added

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = 
+{
+  "extends": "next/core-web-vitals",
+  "rules": {
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-max-props-per-line": [
+      1,
+      {
+        "maximum": 6
+      }
+    ]
+  }
+}
+
