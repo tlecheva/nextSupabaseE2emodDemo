ac5e4d349dd4e3550c4ceae62f1b066a96f106ef
tlechevalier <thomas.lechevalier.ext@inetum.com>
2024-07-09 12:18:24 +0200

    Adding sidebar option to stay sticky

--- components/PhaseAndStatusStepper.tsx
+++ components/PhaseAndStatusStepper.tsx
@@ -1,45 +1,25 @@
-import * as React from "react";
-import * as ReactDom from "react-dom";
-import { StepperComponent, StepsDirective, StepDirective } from '@syncfusion/ej2-react-navigations';
+import * as React from 'react';
 import {
-    Check as CheckIcon,
-    BlurCircular as BlurCircularIcon,
-} from "@airbus/icons/react";
+  StepperComponent,
+  StepsDirective,
+  StepDirective,
+} from '@syncfusion/ej2-react-navigations';
 
 export const PhaseAndStatusStepper = () => {
-    // const getTemplate = ({ step }) => {
-    //     console.log("🚀 ~ getTemplate ~ step:", step)
-    //     const {
-    //         label, isValid, status
-    //     } = step;
-    //     console.log("🚀 ~ getTemplate ~ label, isValid, status:", label, isValid, status)
-    //     const tailwindcss = "rounded-full  text-white text-4xl"
-
-    //     const finalClass = status === 'Completed' ? tailwindcss.replace('grey', 'green') : status === 'InProgress' ? tailwindcss.replace('grey', 'blue') : tailwindcss;
-    //     return (
-    //         <div className="stepper-template-content">
-    //             {label === 'Initialisation' ?
-    //                 <CheckIcon
-    //                     // className="rounded-full bg-green-600 p-2 hover:bg-green-700 text-white text-4xl" />
-    //                     className={finalClass} /> :
-    //                 label === 'Evaluation' ? <BlurCircularIcon className={finalClass} /> :
-    //                     label === 'Investigation' ? <BlurCircularIcon className={finalClass} /> :
-    //                         <BlurCircularIcon className={finalClass} />
-    //             }
-    //             <span className="e-label">{step.label}</span>
-    //         </div>)
-    // }
-
-    return (
-        <div className="stepper-validation">
-            <StepperComponent activeStep={3}>
-                <StepsDirective>
-                    <StepDirective iconCss={'sf-icon-cart'} isValid={true} label={'Initialisation'} />
-                    <StepDirective iconCss={'sf-icon-transport'} label={'Evaluation'} />
-                    <StepDirective iconCss={'sf-icon-payment'} label={'Investigation'} />
-                    <StepDirective iconCss={'sf-icon-cart'} label={'Implementation'} />
-                </StepsDirective>
-            </StepperComponent>
-        </div>
-    )
-}
+  return (
+    <div className="stepper-validation">
+      <StepperComponent activeStep={3}>
+        <StepsDirective>
+          <StepDirective
+            iconCss={'sf-icon-cart'}
+            isValid={true}
+            label={'Initialisation'}
+          />
+          <StepDirective iconCss={'sf-icon-transport'} label={'Evaluation'} />
+          <StepDirective iconCss={'sf-icon-payment'} label={'Investigation'} />
+          <StepDirective iconCss={'sf-icon-cart'} label={'Implementation'} />
+        </StepsDirective>
+      </StepperComponent>
+    </div>
+  );
+};
--- components/SideBarAndContent.tsx
+++ components/SideBarAndContent.tsx
@@ -1,58 +1,113 @@
-
 import * as React from 'react';
-import { ColorModeProvider, IconButton, Typography } from '@airbus/components-react';
 import {
-    Menu as MenuIcon
-} from "@airbus/icons/react";
+  ColorModeProvider,
+  IconButton,
+  Typography,
+} from '@airbus/components-react';
+import {
+  Menu as MenuIcon,
+  ArrowLeft as ArrowLeftIcon,
+  Chevronleft as ChevronLeftIcon,
+  Chevronright as ChevronRightIcon,
+  PlaylistAddCheck as PlaylistAddCheckIcon,
+} from '@airbus/icons/react';
 
 import { SideBarContent } from './SideBarContent';
 
-
 function SideBarAndContent() {
-    const sideBarTitles = //['Attributes', 'Multi-standard', 'Sketches & Comments', '..', 'Master Schedule', '..etc..']
-        [
-            { "title": "Attributes", component: 'Attributes' },
-            { "title": "Multi-standard" },
-            { "title": "Sketches & Comments" },
-            { "title": "..." },
-            { "title": "Master Schedule", component: 'MasterSchedule' },
-            { "title": "..etc." }
-        ]
-    const [showSideBar, setShowSideBar] = React.useState<boolean>(true)
-    const [component, setComponent] = React.useState<string>(sideBarTitles[0].title)
-    const onClickMenuSidebar = () => {
-        setShowSideBar(!showSideBar)
-    }
-    const onSideBarMenuClick = (e: React.MouseEvent<HTMLDivElement>) => {
-        // find in sideBarTitles the component with match the title and set it to component
-        const title = e.currentTarget.innerText
-        const clickedComponent = sideBarTitles.find((sbTitle) => sbTitle.title === title)?.component
-        setComponent(clickedComponent || '')
-    }
-    return (
-        <div className="sidebar-container">
-            <aside className={`sidebar-items${showSideBar ? '' : ' sidebar-collapsed'}`}>
-                <ColorModeProvider mode="dark">
-                    <div className="sidebar-items-right-aligned" onClick={onClickMenuSidebar}>
-                        <IconButton variant="ghost" aria-label="Search">
-                            <MenuIcon />
-                        </IconButton>
-                    </div>
-                    {showSideBar && sideBarTitles.map((sbTitle, i) => (
-                        <Typography key={sbTitle.title} variant={"medium"} 
-                            className={`mr-5 
-                                ${sbTitle.component ? "hover:bg-blue-500 cursor-pointer" : ""}
-                                ${component === sbTitle.component ? "bg-blue-700" : ""}
+  const sideBarTitles =
+    //['Attributes', 'Multi-standard', 'Sketches & Comments', '..', 'Master Schedule', '..etc..']
+    [
+      { title: 'Attributes', component: 'Attributes' },
+      { title: 'Multi-standard' },
+      { title: 'Sketches & Comments' },
+      { title: '...' },
+      { title: 'Master Schedule', component: 'MasterSchedule' },
+      { title: '..etc.' },
+    ];
+  const [showSideBar, setShowSideBar] = React.useState<boolean>(true);
+  const [stickySideBar, setStickySideBar] = React.useState<boolean>(false);
+  const [component, setComponent] = React.useState<string>(
+    sideBarTitles[0].title,
+  );
+  const [title, setTitle] = React.useState<string>(sideBarTitles[0].title);
+
+  const onClickMenuSidebar = () => {
+    setShowSideBar(!showSideBar);
+    setStickySideBar(false);
+  };
+  const onClickSticky = () => {
+    setStickySideBar(!stickySideBar);
+  };
+  const onHoveringOnClosedSidebar = () => {
+    if (stickySideBar) return;
+    if (!showSideBar) setShowSideBar(true);
+  };
+  const onLeavingOpenSidebar = () => {
+    if (stickySideBar) return;
+    if (showSideBar) setShowSideBar(false);
+  };
+  const onSideBarMenuClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    // find in sideBarTitles the component with match the title and set it to component
+    const title = e.currentTarget.innerText;
+    setTitle(title);
+    const clickedComponent = sideBarTitles.find(
+      sbTitle => sbTitle.title === title,
+    )?.component;
+    setComponent(clickedComponent || '');
+  };
+  return (
+    <div className="sidebar-container">
+      <aside
+        className={`sidebar-items${showSideBar ? '' : ' sidebar-collapsed'}`}
+        onMouseEnter={onHoveringOnClosedSidebar}
+        onMouseLeave={onLeavingOpenSidebar}
+      >
+        <ColorModeProvider mode="dark">
+          <div className="sidebar-items-right-aligned flex-col">
+            <div className="flex">
+              <IconButton
+                variant="ghost"
+                aria-label="Search"
+                tooltip="Sticky sidebar"
+                onClick={onClickMenuSidebar}
+              >
+                <ChevronLeftIcon />
+              </IconButton>
+              <IconButton
+                onClick={onClickSticky}
+                variant="ghost"
+                aria-label="Menu"
+                tooltip="Sticky sidebar"
+              >
+                {!stickySideBar && <ChevronRightIcon />}
+                <MenuIcon />
+              </IconButton>
+            </div>
+
+            {!showSideBar && (
+              <div className="sidebar-vertical-text">{title}</div>
+            )}
+          </div>
+          {showSideBar &&
+            sideBarTitles.map((sbTitle, i) => (
+              <Typography
+                key={sbTitle.title}
+                variant={'medium'}
+                className={`mr-5 
+                                ${sbTitle.component ? 'hover:bg-blue-500 cursor-pointer' : ''}
+                                ${component === sbTitle.component ? 'bg-blue-700' : ''}
                             `}
-                            onClick={sbTitle.component ? onSideBarMenuClick : undefined}>
-                            {sbTitle.title}
-                        </Typography>
-                    ))}
-                </ColorModeProvider>
-            </aside>
-            <SideBarContent showSideBar={showSideBar} component={component} />
-        </div>
-    )
-};
+                onClick={sbTitle.component ? onSideBarMenuClick : undefined}
+              >
+                {sbTitle.title}
+              </Typography>
+            ))}
+        </ColorModeProvider>
+      </aside>
+      <SideBarContent showSideBar={showSideBar} component={component} />
+    </div>
+  );
+}
 
 export default SideBarAndContent;
